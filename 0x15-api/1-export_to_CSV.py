7ae931321a7ba1bc7f91d553ddbebc810373075f
abrhamgg <abrhamgg@gmail.com>
2022-10-17 17:00:49 +0300

    second task

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""fetch information using APi"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    API = 'https://jsonplaceholder.typicode.com/'
+    todos = requests.get('{}todos?userId={}'.format(API, sys.argv[1]))
+    user = requests.get('{}users/{}'.format(API, sys.argv[1]))
+    username = user.json()['username']
+    completed = requests.get('{}/todos?userId={}&completed=true'.format
+                             (API, sys.argv[1]))
+
+    with open('{}.csv'.format(sys.argv[1]), 'w') as csv:
+        for i in todos.json():
+            output = '"{}","{}","{}","{}"'.format(sys.argv[1],
+                                                  username, i['completed'],
+                                                  i['title'])
+            csv.write(output)
+            csv.write('\n')
